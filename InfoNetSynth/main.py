14b15e2a5619fcb0beaa27a2685547af861d0814
Spoorthi M G <107809582+spoo-2003@users.noreply.github.com>
2024-04-04 22:59:19 +0530

    Add files via upload

---
+++ InfoNetSynth/main.py
@@ -0,0 +1,70 @@
+import os
+import streamlit as st
+import pickle
+import time
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.llms import CTransformers
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import WebBaseLoader
+st.title("RockyBot: News Research Tool 📈")
+st.sidebar.title("News Article URLs")
+urls = []
+for i in range(3):
+    url = st.sidebar.text_input(f"URL {i+1}")
+    urls.append(url)
+process_url_clicked = st.sidebar.button("Process URLs")
+
+file_path = "faiss_store_openai.pkl"
+
+main_placeholder = st.empty()
+
+llm = CTransformers(
+        model = "llama-2-7b-chat.ggmlv3.q8_0.bin",
+        model_type="llama",
+        max_new_tokens = 512,
+        temperature = 0.5
+    )
+
+if process_url_clicked:
+    # load data
+    loader = WebBaseLoader(urls[0])
+    main_placeholder.text("Data Loading...Started...✅✅✅")
+    data = loader.load()
+    # split data
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=['\n\n', '\n', '.', ','],
+        chunk_size=1000
+    )
+    main_placeholder.text("Text Splitter...Started...✅✅✅")
+    docs = text_splitter.split_documents(data)
+    embeddings = HuggingFaceEmbeddings()
+    vectorstore_openai = FAISS.from_documents(docs, embeddings)
+    main_placeholder.text("Embedding Vector Started Building...✅✅✅")
+    time.sleep(2)
+
+    with open(file_path, "wb") as f:
+        pickle.dump(vectorstore_openai, f)
+    
+query = main_placeholder.text_input("Question: ")
+if query:
+    if os.path.exists(file_path):
+        with open(file_path, "rb") as f:
+            vectorstore = pickle.load(f)
+            st.write("Hi i am ready")
+            chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
+            result = chain({"question": query}, return_only_outputs=True)
+            # result will be a dictionary of this format --> {"answer": "", "sources": [] }
+            st.header("Answer")
+            st.write(result["answer"])
+
+            # Display sources, if available
+            sources = result.get("sources", "")
+            if sources:
+                st.subheader("Sources:")
+                sources_list = sources.split("\n")  # Split the sources by newline
+                for source in sources_list:
+                    st.write(source)
